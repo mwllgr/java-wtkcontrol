8a38d7596b79332d948e445416eb2e9b168d3332
mwllgr <marc@wallinger-online.at>
2020-04-15 16:06:45 +0200

    Fix static classes

--- src/at/mwllgr/wtkcontrol/globals/CommandModes.java
+++ src/at/mwllgr/wtkcontrol/globals/CommandModes.java
@@ -1,8 +1,11 @@
 package at.mwllgr.wtkcontrol.globals;
 
-public abstract class CommandModes {
-    static final byte WRITE_MEMORY = 0x13;
-    static final byte WRITE_DATETIME = 0x14;
-    static final byte READ_MEMORY = 0x15;
-    static final byte LOGGER = 0x18; // Unknown use-case
+public final class CommandModes {
+    private CommandModes() { }
+
+    public static final byte WRITE_MEMORY = 0x13;
+    public static final byte WRITE_DATETIME = 0x14;
+    public static final byte READ_MEMORY = 0x15;
+    public static final byte LOGGER = 0x18; // Unknown use-case
+    public static final byte[] WAKEUP = { 0x41, 0x54, 0x0D };
 }
--- src/at/mwllgr/wtkcontrol/globals/ResponseModes.java
+++ src/at/mwllgr/wtkcontrol/globals/ResponseModes.java
@@ -1,7 +1,9 @@
 package at.mwllgr.wtkcontrol.globals;
 
-public class ResponseModes {
-    static final byte LOGGER = 0x03; // Unknown use-case
-    static final byte WRITE_RESPONSE = 0x11;
-    static final byte READ_RESPONSE = 0x17;
+public final class ResponseModes {
+    private ResponseModes() { }
+
+    public static final byte LOGGER = 0x03; // Unknown use-case
+    public static final byte WRITE_RESPONSE = 0x11;
+    public static final byte READ_RESPONSE = 0x17;
 }
