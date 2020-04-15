379ff6a1ef589017103781fea1f0b32180a25b01
mwllgr <marc@wallinger-online.at>
2020-04-15 16:21:08 +0200

    Add HashMap to Repository

--- src/at/mwllgr/wtkcontrol/model/Repository.java
+++ src/at/mwllgr/wtkcontrol/model/Repository.java
@@ -1,9 +1,10 @@
 package at.mwllgr.wtkcontrol.model;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class Repository {
-
+    HashMap<String, DataField> fields;
     private File addressList;
 
     public File getAddressList() {
@@ -11,6 +12,11 @@ public class Repository {
     }
 
     public void setAddressList(File addressList) {
+        if(fields == null) {
+            fields = new HashMap<>();
+        }
         this.addressList = addressList;
     }
+
+
 }
