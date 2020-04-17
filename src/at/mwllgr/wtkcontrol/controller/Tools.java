457fc83721a49de396282cc9deb3064dbdac49bf
mwllgr <marc@wallinger-online.at>
2020-04-17 21:23:17 +0200

    Add comments in Tools

--- src/at/mwllgr/wtkcontrol/controller/Tools.java
+++ src/at/mwllgr/wtkcontrol/controller/Tools.java
@@ -1,11 +1,18 @@
 package at.mwllgr.wtkcontrol.controller;
 
-import java.math.BigInteger;
-
+/**
+ * This class contains neat functions that are used throughout the program.
+ */
 public final class Tools {
     private Tools() { }
 
+    /**
+     * Converts a hex input string to a byte array
+     * @param input Hex input string
+     * @return Converted bytes
+     */
     public static byte[] hexStringToByteArray(String input) {
+        // Source: https://stackoverflow.com/a/39970426
         int len = input.length();
 
         if (len == 0) {
@@ -30,19 +37,36 @@ public final class Tools {
         return data;
     }
 
+    /**
+     * Converts a byte array into a hex string.
+     * @param bytes Bytes to convert
+     * @param spaces true = space after each group
+     * @return Byte array as hexadecimal string
+     */
     public static String getByteArrayAsHexString(byte[] bytes, boolean spaces) {
         StringBuilder sb = new StringBuilder();
         for (byte currByte : bytes) {
             sb.append(String.format("%02X", currByte));
             if(spaces) sb.append(" ");
         }
+
         return sb.toString().trim();
     }
 
+    /**
+     * Converts a hex string into an int.
+     * @param hex String to convert
+     * @return Converted integer
+     */
     public static int hexStringToInt(String hex) {
         return Integer.parseInt(hex, 16);
     }
 
+    /**
+     * Converts an int into a hex string.
+     * @param hex int to convert
+     * @return int as hexadecimal string
+     */
     public static String intToHexString(int hex) {
         return String.format("%02X", hex);
     }
