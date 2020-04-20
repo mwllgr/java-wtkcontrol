9f002eb44a0046ffa44b637f63d45021db130668
mwllgr <marc@wallinger-online.at>
2020-04-20 19:51:26 +0200

    Add fromString to TimeWith24

--- src/at/mwllgr/wtkcontrol/model/TimeWith24.java
+++ src/at/mwllgr/wtkcontrol/model/TimeWith24.java
@@ -1,14 +1,21 @@
 package at.mwllgr.wtkcontrol.model;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TimeWith24 {
     static final int MIN = 0;
     static final int MAX_HOURS = 24;
     static final int MAX_MINUTES = 59;
     static final int MAX_SECONDS = 59;
+    static final String TIME_REGEX = "^\\d\\d:\\d\\d:\\d\\d$";
 
     int hours;
     int minutes;
     int seconds;
+
+    public TimeWith24() { }
 
     public TimeWith24(int hours, int minutes, int seconds) {
         if(!(this.setHours(hours) && this.setMinutes(minutes) && this.setSeconds(seconds))) {
@@ -55,5 +62,23 @@ public class TimeWith24 {
     @Override
     public String toString() {
         return String.format("%02d:%02d:%02d", this.getHours(), this.getMinutes(), this.getSeconds());
+    }
+
+    public boolean fromString(String time) {
+        Pattern pattern = Pattern.compile(TIME_REGEX);
+        Matcher matcher = pattern.matcher(time);
+
+        if(matcher.find())
+        {
+            String[] timeValues = time.split(":");
+            int hours = Integer.parseInt(timeValues[0]);
+            int minutes = Integer.parseInt(timeValues[1]);
+            int seconds = Integer.parseInt(timeValues[2]);
+            return this.setHours(hours) && this.setMinutes(minutes) && this.setSeconds(seconds);
+        }
+        else
+        {
+            return false;
+        }
     }
 }
