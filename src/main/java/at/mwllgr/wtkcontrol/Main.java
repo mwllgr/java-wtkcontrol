7ef1b7d6fd23cdaf91d65ef17377918c052943ca
mwllgr <mwllgr@inscomers.net>
2022-01-03 01:49:35 +0100

    Add Maven and JavaFX libraries

--- src/main/java/at/mwllgr/wtkcontrol/Main.java
+++ src/main/java/at/mwllgr/wtkcontrol/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
         primaryStage.setTitle("WtkControl");
         primaryStage.setScene(new Scene(root, 750, 500));
         primaryStage.show();
---
+++ src/main/java/at/mwllgr/wtkcontrol/MainWrapper.java
@@ -0,0 +1,7 @@
+package at.mwllgr.wtkcontrol;
+
+public class MainWrapper {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
