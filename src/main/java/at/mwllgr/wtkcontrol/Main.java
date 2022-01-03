7dfe68739ba4bf39578c3a67b4924163713e6ccc
mwllgr <mwllgr@inscomers.net>
2022-01-03 01:51:27 +0100

    Add info about MainWrapper

--- src/main/java/at/mwllgr/wtkcontrol/Main.java
+++ src/main/java/at/mwllgr/wtkcontrol/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Use MainWrapper instead of this class for running the program!
+ */
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
--- src/main/java/at/mwllgr/wtkcontrol/MainWrapper.java
+++ src/main/java/at/mwllgr/wtkcontrol/MainWrapper.java
@@ -1,5 +1,9 @@
 package at.mwllgr.wtkcontrol;
 
+/**
+ * Needed for JavaFX to allow a proper application start.
+ * Run the program using this class!
+ */
 public class MainWrapper {
     public static void main(String[] args) {
         Main.main(args);
